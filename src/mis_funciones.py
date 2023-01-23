9c66b9bd03533bb2fe259fd5aa24eeedc16e6714
aromegar <a_romegar@LAPTOP-2137LP05.localdomain>
2023-01-23 18:29:59 +0100

    subo el JN y el src

---
+++ src/mis_funciones.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+def suma(a,b):
+
+    """
+    Recibe dos float y devuelve la suma
+    """
+    result = a+b
+    
+    return result
+
+
+def resta(a,b):
+    """recibe dos float y resta el segundo al primero
+
+    Args:
+        a (float): cualquier numero decimal, positivo o negativo
+        b (float): cualquier numero decimal, positivo o negativo
+    """
+    return (a-b)
